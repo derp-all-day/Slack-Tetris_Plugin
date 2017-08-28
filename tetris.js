7a6b035abeb78a53d31cffdb81eca4b7f36625c0
Andrew <andrew.blalock.ops@gmail.com>
2017-08-28 17:03:22 -0500

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-	$("<script></script>").appendTo('head').html('$(document).ready(function(){ \
-		$("*").mouseover(function() {$(".message_content:contains(\'slackbot\'):contains(\'/tetris\')").each(function(){$(this).find(".message_body").html("<iframe src=https://fwt.bumxu.com/ style=\'height: 700px;width: 910px;\'></iframe>");});}); \
-	})');
+	$("<script></script>").appendTo('head').html(' \
+	$("#msgs_div").bind("DOMNodeInserted", function(event){ \
+		if (event.target.tagName == "TS-MESSAGE"){ \
+			$(".message_content:contains(\'slackbot\'):contains(\'/tetris\')", this).each(function(){$(this).find(".message_body").html("<iframe src=https://fwt.bumxu.com/ style=\'height: 700px;width: 910px;\'></iframe>");$("div.ql-editor").text("");}); \
+		} \
+	});');
 });
